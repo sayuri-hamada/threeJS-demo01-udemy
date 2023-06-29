6d233dd08f493b231637ac586a7766eb160179f4
sayuri.hamada.zizo <hamada@zizo.ne.jp>
2023-06-29 17:40:05 +0900

    ジオメトリを4つ作成して描画しよう

--- main.js
+++ main.js
@@ -34,4 +34,39 @@ const renderer = new THREE.WebGL1Renderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(window.devicePixelRatio);
 
+// オブジェクトの作成
+// マテリアル
+const material = new THREE.MeshPhysicalMaterial({
+  color: '#3c94d7',
+  metalness: 0.86,
+  roughness: 0.37,
+  flatShading: true,
+});
+
+// メッシュ
+const mesh1 = new THREE.Mesh(
+  new THREE.TorusGeometry(1, 0.4, 16, 60),
+  material
+);
+const mesh2 = new THREE.Mesh(
+  new THREE.OctahedronGeometry(),
+  material
+);
+const mesh3 = new THREE.Mesh(
+  new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+  material
+);
+const mesh4 = new THREE.Mesh(
+  new THREE.IcosahedronGeometry(),
+  material
+);
+
+// 回転ように配置
+mesh1.position.set(2, 0, 0);
+mesh2.position.set(-1, 0, 0);
+mesh3.position.set(2, 0, -6);
+mesh4.position.set(5, 0, 3);
+
+scene.add(mesh1, mesh2, mesh3, mesh4);
+
 renderer.render(scene, camera);
