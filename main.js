9df993427e83ce183236dd1b8ebbaeefd8b47368
sayuri.hamada.zizo <hamada@zizo.ne.jp>
2023-06-29 17:32:06 +0900

    Three.jsの不可欠な3要素を追加しよう

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
 import './style.css'
 import * as THREE from 'three';
 
-console.log(THREE);
+// キャンバスの取得
+const canvas = document.querySelector('.webgl');
+
+/**
+ * 必須の３要素を追加
+ */
+// シーン
+const scene = new THREE.Scene();
+
+// サイズ設定
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+// カメラ
+const camera = new THREE.PerspectiveCamera(
+  35,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 6;
+scene.add(camera);
+
+// レンダラー
+const renderer = new THREE.WebGL1Renderer({
+  canvas: canvas,
+  alpha: true
+})
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+renderer.render(scene, camera);
